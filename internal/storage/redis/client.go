5a62fbef99cb58496ee68caed011b8e2e6851641
zukreindev <ardasamedcelik@gmail.com>
2026-03-11 12:09:11 +0300

    feat: add Redis client and key management functions

---
+++ internal/storage/redis/client.go
@@ -0,0 +1,24 @@
+package redis
+
+import (
+	"context"
+	goredis  "github.com/redis/go-redis/v9"
+)
+
+type Client struct {
+	*goredis.Client
+}
+
+func NewClient(addr, password string, db int) (*Client, error) {
+	rdb := goredis.NewClient(&goredis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
+
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		return nil, err
+	}
+
+	return &Client{Client: rdb}, nil
+}
---
+++ internal/storage/redis/keys.go
@@ -0,0 +1,19 @@
+package redis 
+
+import "fmt"
+
+func WaitingKey(name string) string {
+	return fmt.Sprintf("queue:%s:waiting", name)
+}
+
+func ProcessingKey(name string) string {
+	return fmt.Sprintf("queue:%s:processing", name)
+}
+
+func FailedKey(name string) string {
+	return fmt.Sprintf("queue:%s:failed", name)
+}
+
+func JobKey(id string) string {
+	return fmt.Sprintf("job:%s", id)
+}
